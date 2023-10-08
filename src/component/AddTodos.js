befe06aa8568eaf23c2855b8baba5dede7cc75de
adityaranjan858 <adityaranjan858@gmail.com>
2023-10-09 01:21:39 +0530

    changes of input field to make responsive in mobile view

--- src/component/AddTodos.js
+++ src/component/AddTodos.js
@@ -18,9 +18,9 @@ const AddTodos = () => {
     return (
         <>
             <form onSubmit={submitHandler}>
-                <div className="input-group w-50 m-auto">
+                <div className={`input-group m-auto ${style.input_field_container}`} >
                     <input type="text" autoComplete='on' className={`form-control ${style.input_field}`} id={style.inputID} placeholder="Enter a Todo here..." value={value} onChange={inputHandler} />
-                    <div className="input-group-append">
+                    <div>
                         <button className="btn btn-success ms-2" disabled={value.length === 0} type='submit'>Add a Todo</button>
                     </div>
                 </div>
