9c0e14b050f8c230d628471521a6ab6ed6acac65
adityaranjan858 <adityaranjan858@gmail.com>
2025-01-31 21:07:07 +0530

    updated index

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { BrowserRouter } from 'react-router-dom';
 
-
+// basename={process.env.PUBLIC_URL}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL} >
+    <BrowserRouter  >
     <Provider store={store}>
     <App />
     </Provider>
